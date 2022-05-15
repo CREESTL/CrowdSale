921ef84c5fce8d0356e3b1042761bc9d8fcbbe0b
CREESTL <kroymw3@yandex.ru>
2022-05-15 22:44:09 +0300

    Add First Test

--- test/sample-test.js
+++ test/sample-test.js
@@ -1,19 +1,12 @@
 const { expect } = require("chai");
 const { ethers } = require("hardhat");
 
-describe("Greeter", function () {
-  it("Should return the new greeting once it's changed", async function () {
-    const Greeter = await ethers.getContractFactory("Greeter");
-    const greeter = await Greeter.deploy("Hello, world!");
-    await greeter.deployed();
+describe("TTT", function () {
+  it("Should be named TTT", async function () {
+    const _ttt = await ethers.getContractFactory("TTT");
+    const ttt = await _ttt.deploy();
+    await ttt.deployed();
 
-    expect(await greeter.greet()).to.equal("Hello, world!");
-
-    const setGreetingTx = await greeter.setGreeting("Hola, mundo!");
-
-    // wait until the transaction is mined
-    await setGreetingTx.wait();
-
-    expect(await greeter.greet()).to.equal("Hola, mundo!");
+    expect(await ttt.name()).to.equal("Tasty Tomato Token");
   });
 });
