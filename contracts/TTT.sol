3c08339affa5f06a3cf7c738796493c3bd7a4470
CREESTL <kroymw3@yandex.ru>
2022-05-15 21:32:16 +0300

    Fix Imports

--- contracts/TTT.sol
+++ contracts/TTT.sol
@@ -1,8 +1,7 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.4;
 
-import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
-import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Burnable.sol";
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 
 contract TTT is ERC20 {
     constructor() ERC20("Tasty Tomato Token", "TTT") {}
