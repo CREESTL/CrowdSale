4e9d4ba7a66e4fa122aafbbd7b80dc49a358540c
CREESTL <kroymw3@yandex.ru>
2022-05-15 21:30:00 +0300

    Add Public Mint

--- contracts/TTT.sol
+++ contracts/TTT.sol
@@ -4,7 +4,11 @@ pragma solidity ^0.8.4;
 import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Burnable.sol";
 
-contract TTT is ERC20, ERC20Burnable {
-    constructor() ERC20("Tasty Tomato Token", "TTT") {
-    }
+contract TTT is ERC20 {
+    constructor() ERC20("Tasty Tomato Token", "TTT") {}
+
+    // Make internal _mint() public
+    function mint(address account, uint amount) public {
+        _mint(account, amount);
+    } 
 }
