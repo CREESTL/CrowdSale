aae5d895be887164acf11e337b21447f55621e8e
CREESTL <kroymw3@yandex.ru>
2022-05-15 17:02:34 +0300

    Add Raw ICO. Separate My ICO.

--- contracts/TTTCrowdsale.sol
+++ contracts/TTTCrowdsale.sol
@@ -1,88 +1,17 @@
+// SPDX-License-Identifier: MIT
 pragma solidity ^0.8.4;
 
 import "./TTT.sol";
+import "./Crowdsale.sol";
+import "./WhitelistedCrowdsale.sol";
 import "@openzeppelin/contracts/utils/math/SafeMath.sol";
 
 
 // A crowdsale ICO contract
-contract TTTCrowdsale {
+contract TTTCrowdsale is WhitelistedCrowdsale {
 
-  using SafeMath for uint256;
-
-  // The token being sold
-  TTT public token;
-
-  // Address where funds are collected (forwarded)
-  address payable public wallet;
-
-  // How many token units a buyer gets per wei
-  uint256 public rate;
-
-  // Amount of wei raised
-  uint256 public weiRaised;
-
-  // Event raised on token purchase
-  event TokenPurchase(
-    address indexed purchaser, // paid for tokens
-    address indexed beneficiary, // got the tokens
-    uint256 value, // weis
-    uint256 amount // amount tokens purchased
-  );
-
-
-  constructor(uint256 _rate, address _wallet, TTT _token) {
-    require(_rate > 0, "Wrong Rate!");
-    require(_wallet != address(0), "Wrong Wallet Address!");
-    require(address(_token) != address(0), "Wrong Token Address!");
-
-    rate = _rate;
-    wallet = payable(_wallet);
-    token = _token;
-  }
-
-
-  fallback() external payable {
-    buyTokens(msg.sender);
-  }
-
-  receive() external payable {
-    buyTokens(msg.sender);
-  }
-
-  // Pre-transaction checks
-  function preTransactionValidate(address _beneficiary, uint256 weiAmount) internal {
-    require(_beneficiary != address(0), "Wrong Beneficiary!");
-    require(weiAmount != 0, "Not Enough Wei!");
-  }
-
-  // Main function 
-  // beneficiary - gets the tokens
-  function buyTokens(address beneficiary) public payable {
-
-    // Get the amount of incoming wei
-    uint256 weiAmount = msg.value;
-
-    preTransactionValidate(beneficiary, weiAmount);
-
-    // Calculate token amount to be created
-    uint256 tokens_to_get = weiAmount.mul(rate);
-
-    // Update the number of raised funds
-    weiRaised = weiRaised.add(weiAmount);
-
-    // Emit tokens to the beneficiary
-    token.transfer(beneficiary, tokens_to_get);
-
-    emit TokenPurchase(
-      msg.sender,
-      beneficiary,
-      weiAmount,
-      tokens_to_get
-    );
-
-    // TODO choose one
-    // wallet.transfer(msg.value);
-    wallet.call{value: msg.value};
-  }
+  constructor(uint256 _rate, address _wallet, TTT _token) 
+    WhitelistedCrowdsale(_rate, _wallet, _token)
+  {}
 
 }
--- contracts/WhitelistedCrowdsale.sol
+++ contracts/WhitelistedCrowdsale.sol
@@ -1,51 +1,60 @@
-pragma solidity ^0.8.0;
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.4;
 
-import "./TTTCrowdsale.sol";
+import "./Crowdsale.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 
 
-// Crowdsale in which only whitelisted users can contribute.
-contract WhitelistedCrowdsale is TTTCrowdsale, Ownable {
+/**
+ * @title WhitelistedCrowdsale
+ * @dev Crowdsale in which only whitelisted users can contribute.
+ */
+contract WhitelistedCrowdsale is Crowdsale, Ownable {
 
-  // Whitelist of addresses
   mapping(address => bool) public whitelist;
 
-  
-  // Reverts if beneficiary is not whitelisted. Can be used when extending this contract.
+  /**
+   * @dev Reverts if beneficiary is not whitelisted. Can be used when extending this contract.
+   */
   modifier isWhitelisted(address _beneficiary) {
     require(whitelist[_beneficiary]);
     _;
   }
 
+  constructor(uint256 _rate, address _wallet, TTT _token) Crowdsale (_rate, _wallet, _token){}
 
-  constructor(uint256 _rate, address _wallet, TTT _token) {
-    require(_rate > 0, "Wrong Rate!");
-    require(_wallet != address(0), "Wrong Wallet Address!");
-    require(address(_token) != address(0), "Wrong Token Address!");
 
-    rate = _rate;
-    wallet = payable(_wallet);
-    token = _token;
-  }
-
-  // Adds single address to whitelist.
-  // _beneficiary - address to be added to the whitelist
+  /**
+   * @dev Adds single address to whitelist.
+   * @param _beneficiary Address to be added to the whitelist
+   */
   function addToWhitelist(address _beneficiary) external onlyOwner {
     whitelist[_beneficiary] = true;
   }
 
-  // Removes single address from whitelist.
-  // _beneficiary - address to be removed to the whitelist
+
+  /**
+   * @dev Removes single address from whitelist.
+   * @param _beneficiary Address to be removed to the whitelist
+   */
   function removeFromWhitelist(address _beneficiary) external onlyOwner {
     whitelist[_beneficiary] = false;
   }
 
-
-  // Extend parent behavior requiring beneficiary to be in whitelist.
-  // _beneficiary Token beneficiary
-  // _weiAmount Amount of wei contributed
-  function _preTransactionValidate(address _beneficiary, uint256 _weiAmount) internal isWhitelisted(_beneficiary) {
-    super.preTransactionValidate(_beneficiary, _weiAmount);
+  /**
+   * @dev Extend parent behavior requiring beneficiary to be in whitelist.
+   * @param _beneficiary Token beneficiary
+   * @param _weiAmount Amount of wei contributed
+   */
+  function _preValidatePurchase(
+    address _beneficiary,
+    uint256 _weiAmount
+  )
+    internal
+    override
+    isWhitelisted(_beneficiary)
+  {
+    super._preValidatePurchase(_beneficiary, _weiAmount);
   }
 
 }
