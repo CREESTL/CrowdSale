5767e32ff8230ef04f3864e3b296ff4e3547233c
CREESTL <kroymw3@yandex.ru>
2022-05-17 01:51:18 +0300

    Add Deploy Script

---
+++ scripts/deploy.js
@@ -0,0 +1,29 @@
+// SPDX-License-Identifier: MIT
+const hre = require("hardhat");
+
+async function main() {
+
+  // Get different avaliable addresses
+  [owner, addr1, addr2, ...addrs] = await ethers.getSigners();
+  
+  // We get the contract to deploy
+  // Deploy the token
+  _ttt = await ethers.getContractFactory("TTT");
+  ttt = await _ttt.deploy();
+  await ttt.deployed();
+
+  // Deploy the crowdsale
+  _tttCrowdsale = await ethers.getContractFactory("TTTCrowdsale");
+  tttCrowdsale = await _tttCrowdsale.deploy(owner.address, ttt.address, Date.now());
+  await tttCrowdsale.deployed();
+
+  console.log("TTT deployed to:", ttt.address);
+  console.log("TTTCrowdsale deployed to:", tttCrowdsale.address);
+}
+
+main()
+  .then(() => process.exit(0))
+  .catch((error) => {
+    console.error(error);
+    process.exit(1);
+  });
